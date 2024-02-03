10bae5e8cc1bd1d7fce75873daebd832229e0f7e
Lisoveliy <1986developer@gmail.com>
2024-02-03 15:16:16 +0300

    Refactor, added functions of check pearl

--- src/main/java/by/lisoveliy/TraderListener.java
+++ src/main/java/by/lisoveliy/TraderListener.java
@@ -18,10 +18,19 @@ public class TraderListener implements Listener {
         var entity = event.getEntity();
         if(entity instanceof WanderingTrader trader){
             var discoveredRecipes = trader.getRecipes();
+            //Проверка имеющегося крафта с пёрлами
+            if(discoveredRecipes
+                    .stream()
+                    .anyMatch(recipe -> recipe.getIngredients()
+                            .stream()
+                            .anyMatch(stacks -> stacks.getType() == Material.ENDER_PEARL)))
+                return;
+
             var recipe = new MerchantRecipe(new ItemStack(Material.ENDER_PEARL, 1), Integer.MAX_VALUE);
             recipe.addIngredient(new ItemStack(Material.GOLD_INGOT, new Random().nextInt(1,4)));
-            List<MerchantRecipe> newRecipes = new ArrayList<MerchantRecipe>(discoveredRecipes);
+            List<MerchantRecipe> newRecipes = new ArrayList<>();
             newRecipes.add(recipe);
+            newRecipes.addAll(discoveredRecipes);
             trader.setRecipes(newRecipes);
         }
     }
